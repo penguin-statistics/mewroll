a4b767631ca224b8d34703e81e1131110c5d656e
GalvinGao <galvingaoper@gmail.com>
2021-10-08 14:51:14 +0800

    add deduplication and preprocessing for deleted comments

--- internal/cmd/comment_drawer.go
+++ internal/cmd/comment_drawer.go
@@ -22,6 +22,10 @@ func NewCommentDrawer(conf *CommentDrawerConfig) *CommentDrawer {
 	return &CommentDrawer{conf: conf, client: httpclient.NewDefaultClient()}
 }
 
+func remove(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
+
 func (c *CommentDrawer) Draw() error {
 	fmt.Printf("正在获取想法 %s 的评论内容...\n", c.conf.ThoughtID)
 	comments, err := c.client.GetComments(c.conf.ThoughtID)
@@ -29,7 +33,17 @@ func (c *CommentDrawer) Draw() error {
 		return err
 	}
 
-	fmt.Printf("想法评论内容获取完毕，获取到 %d 条未经去重的评论数量。开始进行去重...\n", len(comments.Entries))
+	fmt.Printf("想法评论内容获取完毕，获取到 %d 条未经处理的评论数量。开始进行预处理...\n", len(comments.Entries))
+
+	newEntries := []httpclient.MewThoughtComment{}
+	for _, entry := range comments.Entries {
+		if !entry.Deleted {
+			newEntries = append(newEntries, entry)
+		}
+	}
+	comments.Entries = newEntries
+
+	fmt.Printf("预处理完毕，获取到 %d 条未经去重的评论数量。开始进行去重...\n", len(comments.Entries))
 
 	var candidatePostIndexes []int
 
@@ -71,18 +85,24 @@ func (c *CommentDrawer) Draw() error {
 	fmt.Printf("想法评论内容去重完毕，去重后的评论数量为 %d。开始抽取...\n", total)
 
 	for i := 0; i < c.conf.Count; i++ {
-		drawResult := randomdrawer.Draw(0, total)
+		currentTotal := len(candidatePostIndexes)
+
+		//fmt.Printf("剩余 %d 条评论。开始抽取...\n", currentTotal)
+
+		drawResult := randomdrawer.Draw(0, currentTotal)
 
 		var comment httpclient.MewThoughtComment
 		for _, entry := range comments.Entries {
-			if entry.Index == drawResult {
+			if entry.Index == candidatePostIndexes[drawResult] {
 				comment = entry
 			}
 		}
 
 		user := comments.Objects.Users[comment.AuthorId]
 
-		fmt.Printf("  - 由想法 %s 抽取的第 #%d 个评论是：由 %s (@%s) 于楼层 #%d 发表的内容「%s」\n", c.conf.ThoughtID, i, user.Name, user.Username, comment.Index, comment.Content)
+		fmt.Printf("  - #%d：由 %s (@%s, %s) 于楼层 #%d 发表的内容「%s」\n", i, user.Name, user.Username, user.Id, comment.Index, comment.Content)
+
+		candidatePostIndexes = remove(candidatePostIndexes, drawResult)
 	}
 
 	return nil
